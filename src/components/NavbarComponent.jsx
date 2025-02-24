f2d7cc0cf074932c3a49dfe065ca32192683df12
Daniel-CZ94 <danielzamacona94@outlook.com>
2025-02-23 19:56:01 -0600

    Se agrego la navbar y los estilos de bootstrap

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,42 @@
+
+const NavbarComponent = () => {
+    return(
+        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-fixed-top">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="./logosirio.png" alt="Logo" width={40} height={40} className="d-inline-block align-text-top"></img>
+                    &nbsp;Joyas Sirio
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Catalogo</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sucursales</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contacto</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Acerca de nosotros</a>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                        </svg>
+
+                    </span>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default NavbarComponent
